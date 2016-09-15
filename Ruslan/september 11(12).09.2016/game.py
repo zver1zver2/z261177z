7cc4d6b7f8d3e825e237b107cc4180dfbf3a9eb7
zver1zver2 <zver1zver2@gmail.com>
2016-09-15 20:28:43 +0300

    a

--- Ruslan/september 11(12).09.2016/game.py
+++ Ruslan/september 11(12).09.2016/game.py
@@ -1,8 +1,44 @@
-input_file = open('input.txt', 'r')
-output_file = open('output.txt', 'r')
+grid = 8
 
-print(input_file.read())
+block = '#'
 
-results = []
+first = []
+second = []
 
-for line in input_file:
+count = 0
+
+for i in range(1,grid+1):
+    count += 1
+    if count == 1:
+        first.append(block)
+    elif count == 2:
+        count = 0
+        first.append(' ')
+
+for i in range(1,grid+1):
+    count += 1
+    if count == 1:
+        second.append(' ')
+    elif count == 2:
+        count = 0
+        second.append(block)
+
+
+f_str = ''
+s_str = ''
+
+
+for i in first:
+    f_str += i
+for c in second:
+    s_str += c
+
+counter = 0
+
+for ban in range(1,grid+1):
+    counter += 1
+    if counter == 1:
+        print(f_str)
+    elif counter == 2:
+        counter = 0
+        print(s_str)
--- Ruslan/september 14.09.2016/triangle.py
+++ Ruslan/september 14.09.2016/triangle.py
@@ -1,4 +1,12 @@
-triangle = ['#', '##', '###', '####', '#####', '######', '#######']
-
-for line in triangle:
-    print(line)
+a = True
+c = 0
+x = input("Enter one symbol/number/string:")
+while a:
+    if len(x) != 1 or x == ' ':
+        print("Try again")
+        a = False
+    else:
+        c+=1
+        print(x*c)
+        if c == 7:
+            a = False
