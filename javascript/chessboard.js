bc0b0d391a073aca1c1cbaff093f79d138480b01
zver1zver2tax3 <zver1zver2tax3@gmail.com>
2016-09-16 16:28:33 +0300

    tax new

--- javascript/chessboard.js
+++ javascript/chessboard.js
@@ -1,11 +1,14 @@
-var chessBoard = function(boardSize, loopType) {
+var chessBoard = function(rows, columns, loopType) {
     var i = 0,
         y = 0;
     switch (loopType) {
         case "for":
-            document.write("</br>Loop 'FOR'</br>");
-            for (i = 0; i < boardSize; i++) {
-                for (y = 0; y < boardSize; y++) {
+            document.getElementById('idH1').innerHTML = 'Loop FOR';
+            //document.write("</br>Loop 'FOR'</br>");
+            var pColumns = new Array();
+            for (i = 0; i < columns; i++) {
+                //pColumns =
+                for (y = 0; y < rows; y++) {
                     document.write("#");
                 }
                 document.write("</br>");
@@ -13,8 +16,8 @@ var chessBoard = function(boardSize, loopType) {
             break;
         case "while":
             document.write("</br>Loop 'WHILE'</br>");
-            while (i < boardSize) {
-                while (y < boardSize) {
+            while (i < columns) {
+                while (y < rows) {
                     document.write("#");
                     y++;
                 }
@@ -29,11 +32,11 @@ var chessBoard = function(boardSize, loopType) {
                 do {
                     document.write("#");
                     y++;
-                } while(y < boardSize);
+                } while(y < rows);
                 i++;
                 y = 0;
                 document.write("</br>");
-            } while(i < boardSize);
+            } while(i < columns);
             break;
         default:
             document.write("Sorry! You didn't select available loops type: FOR, WHILE, DO...WHILE</br>");
