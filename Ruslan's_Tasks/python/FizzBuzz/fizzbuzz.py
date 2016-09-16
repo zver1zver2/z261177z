99fb6bb19dc2566c34c5d537cecc51ae6b184636
makievrus <makievrus@gmail.com>
2016-09-16 19:44:53 +0300

    afgvwteg

--- Ruslan's_Tasks/python/FizzBuzz/fizzbuzz.py
+++ Ruslan's_Tasks/python/FizzBuzz/fizzbuzz.py
@@ -1,11 +1,16 @@
-import random
+try:
+    range_choice1 = int(input('From: '))
+    range_choice2 = int(input('To: '))
 
-for each_number in range(1, 100):
-    if each_number % 3 == 0 and each_number % 5 == 0:
-        print('FizzBuzz')
-    elif each_number % 3 == 0:
-        print('Fizz')
-    elif each_number % 5 == 0:
-        print('Buzz')
-    else:
-        print(each_number)
+    for each_number in range(range_choice1, range_choice2+1):
+        if each_number % 3 == 0 and each_number % 5 == 0:
+            print('FizzBuzz')
+        elif each_number % 3 == 0:
+            print('Fizz')
+        elif each_number % 5 == 0:
+            print('Buzz')
+        else:
+            print(each_number)
+except ValueError:
+    print('Please write numbers!')
+
--- Ruslan's_Tasks/python/non-unique/non-unique.py
+++ Ruslan's_Tasks/python/non-unique/non-unique.py
@@ -1,4 +1,5 @@
 input_list = [2, 2, 1, 3, 2, 1, 5]
+
 print('Input: ', input_list)
 for each_number in input_list:
     if input_list.count(each_number) == 1:
