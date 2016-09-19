3e74d0b80a52c170890fba83180660fc4cebe1ea
zver1zver2 <zver1zver2@gmail.com>
2016-09-19 21:29:08 +0300

    home1

--- lingua/js/jscode.js
+++ lingua/js/jscode.js
@@ -1,5 +1,25 @@
+var pElement = [];
+for(i=0; i<6; i++) {
+  pElement[i] = document.getElementById("p"+(i+1));
+  pElement[i].onmouseover = function(event) {
+    this.style.color="#666";
+    this.style.backgroundColor="#fff";
+  }
+  pElement[i].onmouseout=function(event){
+    this.style.color="#FFF";
+    this.style.backgroundColor="#666";
+  }
+}
 
-document.getElementById("b1").onclick = function(event) {
-    document.getElementById("p1").innerHTML = "Hello World";
-    this.style.color='red';
+var cardElement = new Array();
+for(i=0; i<2; i++) {
+  cardElement[i] = document.getElementById("card"+(i+1));
+  cardElement[i].onmouseover = function(event) {
+    this.style.color="#ccc";
+    this.style.backgroundColor="#666";
+  }
+  cardElement[i].onmouseout = function(event) {
+    this.style.color="#666";
+    this.style.backgroundColor="#ccc";
+  }
 }
